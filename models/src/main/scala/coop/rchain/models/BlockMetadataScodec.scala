2eb5d33bc6de3980afcbe8d185418189636a457b
maximus19910112 <max_permakov1991@mail.ru>
2022-04-18 13:21:11 +0300

    1) Defined BlockMetadataScodec class (analog of BlockMetadata class) (ByteString replaced with Array[Byte]) 2) Defined codec for defined class based on scodec - library

---
+++ models/src/main/scala/coop/rchain/models/BlockMetadataScodec.scala
@@ -0,0 +1,103 @@
+package coop.rchain.models
+
+import scodec.TransformSyntax
+import scodec.bits.ByteVector
+
+final case class JustificationArr(validator: Array[Byte], latestBlockHash: Array[Byte])
+final case class JustificationBV(validator: ByteVector, latestBlockHash: ByteVector)
+
+object MetadataScodec {
+  import scodec.Codec
+  import scodec.codecs._
+
+  private val codecByteArray = {
+    variableSizeBytes(uint8, bytes).xmap[Array[Byte]](_.toArray, ByteVector(_))
+  }
+
+  //  Codecs of Lists and Map should be created through the flatZip method
+  //  (these codecs must know exact count of elements in List/Map containers)
+  private val codecParentsBase = uint16 flatZip { count =>
+    listOfN(provide(count), codecByteArray)
+  }
+  private val codecParents = codecParentsBase.xmap[List[Array[Byte]]]({ case (_, lst) => lst }, {
+    sourceList =>
+      (sourceList.size, sourceList)
+  })
+
+  private val codecJustification = (codecByteArray :: codecByteArray).as[JustificationArr]
+  private val codecJustificationsBase = uint16 flatZip { count =>
+    listOfN(provide(count), codecJustification)
+  }
+
+  private val codecJustifications = codecJustificationsBase.xmap[List[JustificationArr]]({
+    case (_, lst) => lst
+  }, { sourceList =>
+    (sourceList.size, sourceList)
+  })
+
+  private val tupleCodec: Codec[(Array[Byte], Long)] = codecByteArray.pairedWith(int64)
+
+  private val codecWeightMapBase = uint16 flatZip { count =>
+    val tuplesList = listOfN(provide(count), tupleCodec)
+    tuplesList.xmap[Map[Array[Byte], Long]](_.toMap, _.toList)
+  }
+
+  private val codecWeightMap = codecWeightMapBase.xmap[Map[Array[Byte], Long]](
+    { case (_, sourceMap) => sourceMap }, { sourceMap =>
+      (sourceMap.size, sourceMap)
+    }
+  )
+
+  private val codecMetadata =
+    (("hash" | codecByteArray) :: ("parents" | codecParents) ::
+      ("sender" | codecByteArray) :: ("justifications" | codecJustifications) ::
+      ("weightMap" | codecWeightMap) :: ("blockNum" | int64) :: ("seqNum" | int32) :: ("invalid" | bool) ::
+      ("df" | bool) :: ("finalized" | bool)).as[BlockMetadataScodec]
+
+  def encode(block: BlockMetadataScodec): ByteVector =
+    codecMetadata.encode(block).require.toByteVector
+
+  def decode(serializedBlock: ByteVector): BlockMetadataScodec =
+    codecMetadata.decode(serializedBlock.toBitVector).require.value
+}
+final case class BlockMetadataBV(
+    blockHash: ByteVector,
+    parents: List[ByteVector],
+    sender: ByteVector,
+    justifications: List[JustificationBV],
+    weightMap: Map[ByteVector, Long],
+    blockNum: Long,
+    seqNum: Int,
+    invalid: Boolean,
+    directlyFinalized: Boolean,
+    finalized: Boolean
+)
+final case class BlockMetadataScodec(
+    blockHash: Array[Byte],
+    parents: List[Array[Byte]],
+    sender: Array[Byte],
+    justifications: List[JustificationArr],
+    weightMap: Map[Array[Byte], Long],
+    blockNum: Long,
+    seqNum: Int,
+    invalid: Boolean,
+    directlyFinalized: Boolean,
+    finalized: Boolean
+) {
+  def toByteString: ByteVector = MetadataScodec.encode(block = this)
+  def toBlockMetadataBV(): BlockMetadataBV =
+    BlockMetadataBV(
+      ByteVector(blockHash),
+      parents.map(ByteVector(_)),
+      ByteVector(sender),
+      justifications.map(
+        just => JustificationBV(ByteVector(just.validator), ByteVector(just.latestBlockHash))
+      ),
+      weightMap.map { case (bv, num) => (ByteVector(bv), num) },
+      blockNum,
+      seqNum,
+      invalid,
+      directlyFinalized,
+      finalized
+    )
+}
